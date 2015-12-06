3e332ea219d7bc4fa9f39af0b8bbd62a95ad7c07
刘晓龙 <9871208@sin.com>
2015-12-06 13:44:20 +0800

    发生的方式

---
+++ jk/jk/ViewController.m
@@ -0,0 +1,41 @@
+//
+//  ViewController.m
+//  jk
+//
+//  Created by 刘晓龙 on 15/12/6.
+//  Copyright (c) 2015年 hlzx. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    ;';
+    
+    
+    普doubleasda
+    
+    sadasdasa'
+    
+    sadasd
+    
+
+
+
+阿斯顿
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
